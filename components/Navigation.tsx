80ceab5817fe1d04e53925aacbc88f716453e58c
totaldekadens <angelica.moberg@hotmail.com>
2023-03-19 12:09:02 +0100

    fix

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -7,6 +7,14 @@ const Navigation = () => {
       style={{ zIndex: 100 }}
       className="h-20 fixed left-0 right-0 text-gray-700 bg-black flex justify-between px-8 items-center sm:text-xl  md:text-2xl lg:text-4xl lg:h-24"
     >
+      <div>
+        {/*  <Link className="text-white text-sm font-normal" href={"/#projects"}>
+          Projects
+        </Link>
+        <Link className="text-white text-sm font-normal" href={"/aboutme"}>
+          Who am I?
+        </Link> */}
+      </div>
       <div className="flex gap-3">
         <a href="mailto:angelica.moberg@hotmail.com">
           <img src="/letter.png" />
